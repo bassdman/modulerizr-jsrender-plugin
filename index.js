25b2703c686228eaf358408e0462b4a98f03a039
Manuel Gelsen <manuel.gelsen@gmail.com>
2019-12-26 16:56:15 +0100

    refactoring

--- index.js
+++ index.js
@@ -4,29 +4,44 @@ const jsrender = require('jsrender');
 class ModulerizrJsRenderPlugin {
     constructor(pluginconfig = {}) {
         this.internal = true;
-        this.config = pluginconfig;
+        this.pluginconfig = pluginconfig;
+
+        throwErrorIfDelimitersAreNotValid(pluginconfig.delimiters);
     }
     async apply(modulerizr) {
         modulerizr.plugins.on('afterRender', async() => {
-            return modulerizr.store.$each("$.src.*", ($, currentFile, currentPath, i) => {
-                const $components = $('[data-component-instance]');
 
-                $components.each((i, e) => {
-                    const $currentComp = $(e);
-                    const embeddedComponentId = $currentComp.attr('data-component-instance');
-                    const embeddedComponent = modulerizr.store.queryOne(`$.embeddedComponents.id_${embeddedComponentId}`);
-                    const componentTemplate = modulerizr.store.queryOne(`$.component.id_${$currentComp.attr('id')}`);
+            return modulerizr.store.$each("$.src.*/[data-component-instance]", ($currentComp, currentFile, currentPath, i) => {
+                const embeddedComponentId = $currentComp.attr('data-component-instance');
+                const embeddedComponent = modulerizr.store.queryOne(`$.embeddedComponents.id_${embeddedComponentId}`);
+                const componentTemplate = modulerizr.store.queryOne(`$.component.id_${$currentComp.attr('id')}`);
 
-                    const params = Object.assign({}, embeddedComponent.attributes, componentTemplate.data || {});
+                const params = Object.assign({}, embeddedComponent.attributes, componentTemplate.prerenderdata || {});
 
-                    const tmpl = jsrender.templates($currentComp.html()); // Compile template from string
-                    const renderedTemplate = tmpl.render(params);
+                if (this.pluginconfig.delimiters) {
+                    jsrender.views.settings.delimiters(this.pluginconfig.delimiters[0], this.pluginconfig.delimiters[1])
+                }
+                if (this.pluginconfig.allowCode) {
+                    jsrender.views.settings.allowCode(this.pluginconfig.allowCode);
+                }
+                if (this.pluginconfig.debugMode) {
+                    jsrender.views.settings.debugMode(this.pluginconfig.debugMode);
+                }
 
-                    $currentComp.html(renderedTemplate);
-                });
+                const tmpl = jsrender.templates($currentComp.html().replace(/&gt;/g, '>').replace(/&lt;/g, '<').replace(/&amp;/g, "&")); // Compile template from string
+                const renderedTemplate = tmpl.render(params);
+
+                $currentComp.html(renderedTemplate);
             });
         });
     }
 }
 
 exports.ModulerizrJsRenderPlugin = ModulerizrJsRenderPlugin;
+
+function throwErrorIfDelimitersAreNotValid(delimiters) {
+    if (delimiters) {
+        if (!Array.isArray(delimiters) || delimiters.length != 2)
+            throw new Error('Error in ModulerizrJsRenderPlugin(config): config.delimiters must be an array with start- and endtag. Example: ["<$","$>]');
+    }
+}
